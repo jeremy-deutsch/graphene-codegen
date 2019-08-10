8e0399086c11c7bf797c9d4c454d136783b5a38c
Jeremy Deutsch <jeremy.e.deutsch@gmail.com>
2019-08-10 14:23:24 -0700

    add snapshot test

---
+++ src/__tests__/generatePythonStr.test.ts
@@ -0,0 +1,66 @@
+import generatePythonStr from "../generatePythonStr"
+
+// taken from an apollo tutorial
+const sdlStr = `
+type Query {
+  launches(
+    """
+    The number of results to show. Must be >= 1. Default = 20
+    """
+    pageSize: Int
+    """
+    If you add a cursor here, it will only return results _after_ this cursor
+    """
+    after: String
+  ): LaunchConnection!
+  launch(id: ID!): Launch
+  me: User
+}
+type Mutation {
+  # if false, signup failed -- check errors
+  bookTrips(launchIds: [ID]!): TripUpdateResponse!
+  # if false, cancellation failed -- check errors
+  cancelTrip(launchId: ID!): TripUpdateResponse!
+  login(email: String): String # login token
+}
+type TripUpdateResponse {
+  success: Boolean!
+  message: String
+  launches: [Launch]
+}
+"""
+Simple wrapper around our list of launches that contains a cursor to the
+last item in the list. Pass this cursor to the launches query to fetch results
+after these.
+"""
+type LaunchConnection {
+  cursor: String!
+  hasMore: Boolean!
+  launches: [Launch]!
+}
+type Launch {
+  id: ID!
+  site: String
+  mission: Mission
+  rocket: Rocket
+  isBooked: Boolean!
+}
+type Rocket {
+  id: ID!
+  name: String
+  type: String
+}
+type User {
+  id: ID!
+  email: String!
+  trips: [Launch]!
+}
+type Mission {
+  name: String
+  missionPatch(size: String): String
+}
+`
+
+it("generates stable python code", () => {
+  expect(generatePythonStr(sdlStr)).toMatchSnapshot()
+})
